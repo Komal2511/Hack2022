bc5b6146467d895e710a081485247ad2f7446977
Komal Kolani <43901687+Komal2511@users.noreply.github.com>
2022-10-29 12:45:27 +0530

    Revert "Add defination for Armstrong number"

--- Armstrongnumber.py
+++ Armstrongnumber.py
@@ -2,11 +2,11 @@
 """
 Created on Sat Oct 29 12:02:09 2022
 
-@author: Komal And Omkar
+@author: DELL
 """
 
 # Python program to check if the number is an Armstrong number or not
-# An Armstrong number is one whose sum of digits raised to the power three equals the number itself.
+
 # take input from the user
 num = int(input("Enter a number: "))
 
