d9a2c2d35a7f70b06de8700fd2083b00cc84a152
Komal Kolani <43901687+Komal2511@users.noreply.github.com>
2022-10-29 12:04:18 +0530

    Added sqrt.py file

---
+++ sqrt.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 11:56:44 2022
+
+@author: DELL
+"""
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
